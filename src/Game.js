e1e60b49fb8336bca95cd1c6d4fa3e3b9edfdce7
Seth <colts18seth@gmail.com>
2021-03-09 21:20:47 -0500

    added card imgs

--- src/Game.js
+++ src/Game.js
@@ -1,19 +1,20 @@
-import Deck from "./Deck";
-import { getNewDeck } from "./API";
-import { useEffect, useState } from "react";
+// import Deck from "./Deck";
+// import { getNewDeck } from "./API";
+// import { useEffect, useState } from "react";
 function Game() {
-    const [deckId, setDeckId] = useState(null);
+    // const [deckId, setDeckId] = useState(null);
 
-    useEffect(() => {
-        async function getDeckId() {
-            await getNewDeck().then((res) => setDeckId(res.data.deck_id))
-        }
-        getDeckId()
-    }, [])
+    // useEffect(() => {
+    //     async function getDeckId() {
+    //         await getNewDeck().then((res) => setDeckId(res.data.deck_id))
+    //     }
+    //     getDeckId()
+    // }, [])
 
     return (
         <div className="Game">
-            <Deck deckId={deckId} />
+            <img src="../PNG/2C.png" alt="card" />
+            {/* <Deck deckId={deckId} /> */}
         </div>
     );
 }
